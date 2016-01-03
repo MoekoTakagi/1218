eb671d1275ef0bf715a78e76c3aa940265b7cad0
Moeko Takagi <barbiepinkmindoll@gmail.com>
2016-01-03 18:49:15 +0900

    BFSチェック前

--- hw1218-1.c
+++ hw1218-1.c
@@ -11,28 +11,34 @@ d e f -
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 typedef struct Node Node;
 #define MAX_NODES 100000
+#define MAX_STATES 100000 //大き過ぎたらできない
 #define N 16
 #define M 4
 
 struct Node{
 	int posi; //ノードのある位置
-	int list[]; //隣接リスト
+	int list[N]; //隣接リスト *構造体でも配列を使う場合は確保
 };
 
 static int node_used = 0;
 static Node nodes[MAX_NODES];
 static Node* node[N];
 const int final_state[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0}; //ゴール
+static int initial_state[N];
 
+void breadth_first_search();
+bool check_same_state(int queue[MAX_STATES][N], int rear);
 Node* create_node(); //ノード作成
 void create_list(); //隣接リスト作成
 int search_position(int n); //目的の位置にいるピースの数字を返す
 
 int main(){
-	int i, x, initial_state[N];
+	int i, x;
 	char c;
 	for(i = 0; i < N; i++){
 		node[i] = create_node();
@@ -42,27 +48,63 @@ int main(){
 		if(c <= '9' && c >= '1'){
 			x = c - '0';
 			node[x]->posi = i/2;
-		//	printf("x = %d, posi = %d\n", x, node[x]->posi);
-		//	printf("x = %d\n", x);
 		}else if(c >= 'a' && c <= 'f'){
 			x = c + 0x0A - 0x61; //16進数から10進数に変換
 			node[x]->posi = i/2;
-		//	printf("%d\n", x);
-
 		}else if(c == '-'){ //空のピースを0とする
 			node[0]->posi = i/2;
-
 		}
+		
 	}
 	for(i = 0; i < N; i++){ //初期状態
-//		printf("i = %d, posi = %d\n", i, node[i]->posi);
 		initial_state[node[i]->posi] = i;
-	}
-	for(i = 0; i < N; i++){
-		printf("%d ", initial_state[i]);
+//		printf("添字 = %dで posi = %d\n", i, node[i]->posi);
 	}
 	create_list();
+//	breadth_first_search();
 }
+/*
+void breadth_first_search(){
+	int front = 0, rear = 1;
+	int i, j, n, s, p, keep, space[MAX_STATES], queue[MAX_STATES][N]; //space position
+	space[front] = node[0]->posi; //空白の場所の初期状態
+	memcpy(queue[front], initial_state, N); //初期状態をキューに積む
+	printf("ここ\n");
+	n = node[space[front]]->list[0]; //隣接したピースの数字
+	printf("n = %d\n", n);
+	printf("sp = %d\n", space[front]);
+	for(i = 0; n != -1; i++){ //空白からつながるリストを見る
+		printf("あれ\n");
+		s = space[front]; //取り出したキューの空白がある場所
+		p = node[n]->posi; //隣接したピースのある場所
+		memcpy(queue[rear], queue[front], N); //移動した状態をキューに積むために前の状態をコピー
+		queue[rear][s] = n; //0(空白)があった場所に、隣接したピースの数字を代入
+		queue[rear][p] = 0; //隣接したピースがあった場所に、0を代入
+
+		node[s]->posi = p; //空白のあった場所に隣接したピースの位置情報を入れる
+		node[n]->posi = s; //隣接したピースのあった場所に空白の位置情報を入れる
+		space[rear] = p; //空白情報に、隣接したピースの位置情報を代入
+		printf("きた\n");
+
+		if(check_same_state(queue, rear) == true){
+			for(j = 0; j < N; j++){
+				printf("%d ", queue[rear][j]);
+			}
+			printf("\n");
+		}
+	}
+}*/
+
+bool check_same_state(int queue[MAX_STATES][N], int rear){
+	int i;
+	for(i = 0; i <= rear; i++){
+		if(memcpy(queue[i], final_state, N)){
+			return true;
+		}
+	}
+	return false;
+}
+
 
 Node* create_node(){
 	if(node_used == MAX_NODES){
@@ -74,13 +116,18 @@ Node* create_node(){
 
 void create_list(){
 	int i, j, x;
+
 	for(i = 0; i < N; i++){ //リストの初期化
 		for(j = 0; j < M; j++){
 			node[i]->list[j] = -1;
 		}
 	}
 	for(i = 0; i < N; i++){
-		x = node[i]->posi;
+//		printf("添字 = %dで posi = %d\n", i, node[i]->posi);
+	}
+	for(i = 0; i < N; i++){
+		x = node[i]->posi; //ピースの数字iがある場所
+	//	printf("ピース%dのposi = %d\n", i, x);
 		//4角の場合
 		if(x == 0){ //左上
 			node[i]->list[0] = search_position(x+1);
@@ -104,11 +151,12 @@ void create_list(){
 			node[i]->list[0] = search_position(x-1);
 			node[i]->list[1] = search_position(x+1);
 			node[i]->list[2] = search_position(x-M);
-		}else if(x % M == 0){
+		}else if(x % M == 0){ //左側
 			node[i]->list[0] = search_position(x+1);
 			node[i]->list[1] = search_position(x+M);
 			node[i]->list[2] = search_position(x-M);
-		}else if(x % M == 3){
+
+		}else if(x % M == 3){ //右側
 			node[i]->list[0] = search_position(x-1);
 			node[i]->list[1] = search_position(x+M);
 			node[i]->list[2] = search_position(x-M);
@@ -119,6 +167,7 @@ void create_list(){
 			node[i]->list[1] = search_position(x+M);
 			node[i]->list[2] = search_position(x-M);
 		}
+//		printf("添字 = %d, posi = %dで0 = %d, 1 = %d, 2 = %d\n", i, x, node[i]->list[0], node[i]->list[1], node[i]->list[2]);
 	}
 }
 
